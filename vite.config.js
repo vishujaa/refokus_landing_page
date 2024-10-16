5ed40621689fcf5668c64b67f1e70cee8b38a178
vishal jat <vishujat08@gmail.com>
2024-10-16 23:26:48 +0530

    third-c

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "refokus_landing_page"
-})
+  base: '/refokus_landing_page/' // Correct base path for GitHub Pages
+});
